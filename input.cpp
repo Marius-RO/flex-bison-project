2b77b1ce1a7d68e516d2fd3943ba948330dd5b73
Marius-RO <blw_matt@rocketmail.com>
2021-03-27 13:32:54 +0200

    Adauga flex

---
+++ input.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // program care calculeaza suma a doua numere
+    int primulNumar = 20;
+    float alDoileaNumar = 2;
+
+    /*
+        COMENTARIU PE 
+        MAI 
+        MULTE
+        LINII
+    */
+
+    cout << "Suma pentru " << primulNumar << " si " << alDoileaNumar << " este " << primulNumar + alDoileaNumar;
+
+    string variabilaChar = 'a';
+    string variabilaString = "Hello World";
+
+    return 0;
+    
+}
