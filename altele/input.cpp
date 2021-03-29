2f43c84a31a9a7cf53bc42725cd4a7e429cbd253
Marius-RO <blw_matt@rocketmail.com>
2021-03-29 12:09:12 +0300

    Mici modificari

--- altele/input.cpp
+++ altele/input.cpp
@@ -4,21 +4,37 @@ using namespace std;
 
 int main(){
 
+    /**
+
     // program care calculeaza suma a doua numere
     int primulNumar = 20;
     float alDoileaNumar = 2;
-
-    /*
-        COMENTARIU PE 
-        MAI 
-        MULTE
-        LINII
-    */
 
     cout << "Suma pentru " << primulNumar << " si " << alDoileaNumar << " este " << primulNumar + alDoileaNumar;
 
     string variabilaChar = 'a';
     string variabilaString = "Hello World";
+
+    **/
+
+   /**
+    *  COMENTARIU PE 
+        MAI 
+        MULTE
+        LINII
+    **/
+
+    int primulNumar = 20 * 20 / 20 + 1 - 2;
+    float alDoileaNumar = 2;
+
+    if (primulNumar > alDoileaNumar){
+        while(primulNumar > alDoileaNumar){
+            alDoileaNumar = alDoileaNumar + 1;
+        }
+    }
+    else{
+        cout << "Aici in else \n";
+    }
 
     return 0;
     
